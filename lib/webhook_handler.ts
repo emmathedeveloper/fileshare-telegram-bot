d170d910286c9eba172d648dac20eec0011efff9
emmathedeveloper <emmathedeveloper@gmail.com>
2025-10-12 11:45:11 +0100

    Add conflict handling for file record insertion in InsertFileRecord method

--- lib/webhook_handler.ts
+++ lib/webhook_handler.ts
@@ -78,7 +78,7 @@ export default class WebhookHandler {
       uploader_id: message.from?.id.toString() || user.user_telegram_id,
       media_group_id: message.media_group_id || "",
       bot_id,
-    }).returning();
+    }).onConflictDoNothing({ target: [uploaded_files.message_id] }).returning();
 
     return file_record;
   }
